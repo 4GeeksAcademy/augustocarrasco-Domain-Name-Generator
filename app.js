3c6bb56bf70a0632301b466fb359fac83d041331
Augusto Carrasco <142824503+AugustoCarrasco@users.noreply.github.com>
2023-09-10 09:15:29 +0000

    First commit

---
+++ app.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-alert, no-console */
+
+let pronoun = ['the','our'];
+let adj = ['great', 'big' ];
+let noun = ['jogger','racoon'];
+let dom = ['.com','.es','.uk'];
+
+function DomainGenerator(arr1,arr2,arr3,arr4) {
+    let result = [];
+
+}
+
+console.log(DomainGenerator(pronoun,adj,noun,dom));
--- src/app.js
+++ src/app.js
@@ -7,5 +7,27 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let dom = [".com", ".es", ".uk"];
+  const list = document.getElementById("list");
+  function DomainGenerator(arr1, arr2, arr3, arr4) {
+    let result = "";
+    for (let i = 0; i < arr1.length; i++) {
+      for (let j = 0; i < arr2.length; j++) {
+        for (let k = 0; i < arr3.length; k++) {
+          for (let l = 0; i < arr4.length; l++) {
+            result.push([arr1[i] + arr2[j] + arr3[k] + arr4[l]]);
+          }
+        }
+      }
+    }
+    console.log(result);
+    list.innerHTML = "<p>" + +"</p>";
+    Helo;
+  }
+
+  // console.log(DomainGenerator(pronoun, adj, noun, dom));
   console.log("Hello Rigo from the console!");
 };
